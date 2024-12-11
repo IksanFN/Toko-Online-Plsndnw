b0dddb7381da4c1eecf0356cd1b95aa8134fffe7
fajarvrmn <abahripuh@gmail.com>
2024-12-11 15:38:24 +0700

    landing page Shop

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -1,6 +1,6 @@
 <nav class="navbar navbar-expand-lg py-3 bg-white sticky-top shadow-smooth">
     <div class="container">
-        <a class="navbar-brand fw-bold" href="{{ route('dashboard') }}">PLSNDNW.</a>
+        <b><i><a style="font-family: Futura" class="navbar-brand fw-bold" href="{{ route('dashboard') }}">PLSNDNW.</a></i></b>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
